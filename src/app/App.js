d6d41234d38f445a3ef7b5655fc10d6c968bd41b
vadimcimbalyk <vadymonua@gmail.com>
2022-09-02 17:30:11 +0300

    header template

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,24 @@
 import "./app.css"
 
+import beansLogoWhite from '../img/beans-logo-white.svg';
+import beansLogoBlack from '../img/beans-logo-black.svg';
+import solimo from "../img/solimo-coffee-beans.png";
+import presto from "../img/presto-coffee-beans.png";
+import aromico from "../img/aromico-coffee-beans.png"
+
 
 function App() {
     return (
         <div className="App">
             <header>
-                <div className="container">
-                    <nav>
+                <div className="header__container">
+                    <nav className="header__nav">
                         <ul>
                             <li>
-                                <a href="#">our coffee</a>
+                                <a href="#">coffee house</a>
                             </li>
                             <li>
-                                <a href="#">coffe house</a>
+                                <a href="#">our coffee</a>
                             </li>
                             <li>
                                 <a href="#">For your pleasure</a>
@@ -22,7 +28,7 @@ function App() {
                     <div className="container__title">
                         <h1>Everything You Love About Coffee</h1>
 
-                        <img src="" alt=""/>
+                        <img src={beansLogoWhite} alt="beans logo"/>
                         <p>We makes every day full of energy and taste <br/>
                             Want to try our beans?
                         </p>
@@ -35,7 +41,7 @@ function App() {
                     <div className="container__section-first">
                         <h2>About us</h2>
                         <picture>
-                            <img src="" alt=""/>
+                            <img src={beansLogoBlack} alt="beans logo black"/>
                         </picture>
                         <p>Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
                             Afraid at highly months do things on at. Situation recommend objection do intention
@@ -54,7 +60,7 @@ function App() {
                                 <li className="card">
                                     <picture>
                                         <source/>
-                                        <img src="" alt=""/>
+                                        <img src={solimo} alt=""/>
                                     </picture>
                                     <span>Solimo Coffee Beans 2 kg</span>
                                     <span>10.73$</span>
@@ -62,7 +68,7 @@ function App() {
                                 <li className="card">
                                     <picture>
                                         <source/>
-                                        <img src="" alt=""/>
+                                        <img src={presto} alt=""/>
                                     </picture>
                                     <span>Presto Coffee Beans 1 kg</span>
                                     <span>15.99$</span>
@@ -70,7 +76,7 @@ function App() {
                                 <li className="card">
                                     <picture>
                                         <source/>
-                                        <img src="" alt=""/>
+                                        <img src={aromico} alt=""/>
                                     </picture>
 
                                     <span>AROMISTICO Coffee 1 kg</span>
@@ -82,7 +88,24 @@ function App() {
 
                 </div>
             </main>
-            <footer></footer>
+            <footer>
+                <div className="container">
+                    <ul className="footer__nav">
+                        <li>
+                            <a href="#">our coffee</a>
+                        </li>
+                        <li>
+                            <a href="#">coffee house</a>
+                        </li>
+                        <li>
+                            <a href="#">For your pleasure</a>
+                        </li>
+                    </ul>
+                    <picture>
+                        <img src={beansLogoBlack} alt="beans logo black"/>
+                    </picture>
+                </div>
+            </footer>
         </div>
     );
 }
